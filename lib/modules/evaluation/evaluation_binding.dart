c96bd3b538988987491ad4575635235f622a81aa
Eduardo Machado <eduardo.fapro@gmail.com>
2024-02-21 22:02:47 -0300

    .

--- lib/modules/evaluation/evaluation_binding.dart
+++ lib/modules/evaluation/evaluation_binding.dart
@@ -10,17 +10,6 @@ import '../../app/task_instance/task_instance_repository.dart';
 class EvaluationBinding extends Bindings {
   @override
   void dependencies() {
-    // Instantiate EvaluationService with required repositories
-    EvaluationService moduleService = EvaluationService(
-      moduleRepository: Get.find<ModuleRepository>(),
-      taskRepository: Get.find<TaskRepository>(),
-      taskInstanceRepository: Get.find<TaskInstanceRepository>(),
-      moduleInstanceRepository: Get.find<ModuleInstanceRepository>(),
-    );
-
-    // Put EvaluationService into GetX storage to be used globally
-    Get.put<EvaluationService>(moduleService, permanent: true);
-
     // Lazy put EvaluationController into GetX storage, ensuring it can be retrieved later
     Get.lazyPut<EvaluationController>(
           () => EvaluationController(),
