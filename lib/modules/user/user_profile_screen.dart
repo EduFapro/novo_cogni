cb3eb3c91ebe837187699c6a7771ab178eff26ba
Eduardo Machado <eduardo.fapro@gmail.com>
2024-03-07 14:12:17 -0300

    não está fetchando módulos instances

--- lib/modules/user/user_profile_screen.dart
+++ lib/modules/user/user_profile_screen.dart
@@ -3,12 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:novo_cogni/modules/user/user_profile_screen_controller.dart';
 
-class UserProfileScreen extends GetView<UserProfileScreenController>  {
+class UserProfileScreen extends GetView<UserProfileScreenController> {
   const UserProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     double screenHeight = MediaQuery.of(context).size.height;
     double bodyHeight = screenHeight * 0.85;
 
@@ -20,7 +19,16 @@ class UserProfileScreen extends GetView<UserProfileScreenController>  {
         backgroundColor: Colors.black,
       ),
       body: Container(
-
+        child: Column(
+          children: [
+            Text("AVALIADOR"),
+            Text(controller.userAvaliador.value.toString()),
+            Text("Avaliações:"),
+            Text(controller.evaluations.toString()),
+            Text("Instancias de módulo:"),
+            Text(controller.modules.toString())
+          ],
+        ),
       ),
     );
   }
--- lib/modules/user/user_profile_screen_controller.dart
+++ lib/modules/user/user_profile_screen_controller.dart
@@ -1,18 +1,27 @@
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:get/get.dart';
+import 'package:novo_cogni/app/evaluator/evaluator_entity.dart';
+import 'package:novo_cogni/global/user_service.dart';
+
+import '../../app/evaluation/evaluation_entity.dart';
+import '../../app/module_instance/module_instance_entity.dart';
 
 class UserProfileScreenController extends GetxController {
+  final UserService userService = Get.find<UserService>();
+
+  final Rx<EvaluatorEntity?> userAvaliador = Rx<EvaluatorEntity?>(null);
+  RxList<EvaluationEntity> evaluations = <EvaluationEntity>[].obs;
+  RxMap<int, List<ModuleInstanceEntity>> modules = <int, List<ModuleInstanceEntity>>{}.obs;
 
   @override
   void onInit() {
     super.onInit();
-
+    userAvaliador.value = userService.user.value;
+    evaluations.value = userService.evaluations;
+    modules.value = userService.modules;
   }
 
   @override
   void onClose() {
-
     super.onClose();
   }
-
 }
-
