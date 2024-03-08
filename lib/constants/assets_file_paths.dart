2740058d6ec8ff557dff2eee194fdac5ad36a38e
Eduardo Machado <eduardo.fapro@gmail.com>
2024-03-08 14:46:11 -0300

    audio da task 1 rodando

--- lib/constants/assets_file_paths.dart
+++ lib/constants/assets_file_paths.dart
@@ -1,6 +1,6 @@
 class AudioFilePaths {
 
-  static String get qual_o_seu_nome => 'assets/prompts/audio_01.wav';
+  static String get qual_o_seu_nome => 'assets/task_prompts/audio01.wav';
 
   static String get audioPlaceholder => 'assets/audio/audio_placeholder.mp3';
   static String get cincoPatinhos => 'assets/audio/cinco_patinhos.mp3';
