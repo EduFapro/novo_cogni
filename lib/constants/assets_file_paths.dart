2f58f7bf2e7d39412c34d42b25583bfcc49845cc
Eduardo Machado <eduardo.fapro@gmail.com>
2024-03-09 19:47:22 -0300

    Cadastro de atividades Wip

--- lib/constants/assets_file_paths.dart
+++ lib/constants/assets_file_paths.dart
@@ -13,6 +13,50 @@ class AudioFilePaths {
   static String get algum_medico_ja_disse_que_voce_tem_alguma_doenca => 'assets/task_prompts/audio_10.wav';
 
 
+  static String get posso_ver_como_esta_sua_memoria => 'assets/task_prompts/audio_11.wav';
+  static String get agora_vamos_fazer_um_calculo => 'assets/task_prompts/audio_12.wav';
+  static String get em_que_ano_estamos => 'assets/task_prompts/audio_13.wav';
+  static String get em_que_mes_estamos => 'assets/task_prompts/audio_14.wav';
+  static String get em_que_dia_do_mes_estamos => 'assets/task_prompts/audio_15.wav';
+  static String get em_que_dia_da_semana_estamos => 'assets/task_prompts/audio_16.wav';
+  static String get quantos_anos_voce_tem => 'assets/task_prompts/audio_17.wav';
+  static String get em_que_local_nos_estamos_agora => 'assets/task_prompts/audio_18.wav';
+  static String get qual_e_o_nome_do_atual_presidente_do_brasil => 'assets/task_prompts/audio_19.wav';
+  static String get qual_e_o_nome_do_ex_presidente_do_brasil => 'assets/task_prompts/audio_20.wav';
+  static String get agora_voce_ouvira_uma_lista_de_palavras => 'assets/task_prompts/audio_21.wav';
+  static String get perna_jardim_conta_mulher_apartamento => 'assets/task_prompts/audio_22.wav';
+  static String get agora_voce_ouvira_as_mesmas_palavras_novamente => 'assets/task_prompts/audio_23.wav';
+  static String get perna_jardim_conta_mulher_apartamento_carro_jornal_cha_moto_lampada_pode_comecar => 'assets/task_prompts/audio_24.wav';
+  static String get o_que_voce_fez_ontem => 'assets/task_prompts/audio_27.wav';
+  static String get como_era_a_sua_brincadeira_favorita_da_infancia => 'assets/task_prompts/audio_28.wav';
+  static String get agora_voce_devera_falar_todas_as_palavras_que_puder_se_lembrar_da_lista_que_voce_ouviu_antes => 'assets/task_prompts/audio_29.wav';
+  static String get agora_voce_ouvira_uma_pequena_historia => 'assets/task_prompts/audio_30.wav';
+  static String get ana_estava_preocupada_porque_seu_gato_mingau_tinha_desaparecido => 'assets/task_prompts/audio_31.wav';
+  static String get agora_voce_tera_um_minuto_para_falar_o_maior_numero_de_nome_de_animais_que_voce_conhece => 'assets/task_prompts/audio_32.wav';
+  static String get agora_voce_tera_um_minuto_para_falar_o_maior_numero_de_palavras_que_comecem_com_a_letra_f => 'assets/task_prompts/audio_33.wav';
+  static String get agora_voce_tera_um_minuto_para_falar_o_maior_numero_de_palavras_que_comecem_com_a_letra_a => 'assets/task_prompts/audio_34.wav';
+  static String get agora_voce_tera_um_minuto_para_falar_o_maior_numero_de_palavras_que_comecem_com_a_letra_s => 'assets/task_prompts/audio_35.wav';
+  static String get agora_fale_tudo_o_que_voce_esta_vendo_nesta_figura => 'assets/task_prompts/audio_36.wav';
+  static String get agora_conte_qualquer_coisa_que_voce_puder_se_lembrar_sobre_a_historia_que_voce_ouviu_antes => 'assets/task_prompts/audio_37.wav';
+  static String get responda_as_proximas_perguntas_com_sim_ou_nao => 'assets/task_prompts/audio_38.wav';
+  static String get voce_consegue_tomar_banho_sozinho_sem_receber_ajuda => 'assets/task_prompts/audio_39.wav';
+  static String get voce_consegue_se_vestir_sozinho_sem_receber_ajuda => 'assets/task_prompts/audio_40.wav';
+  static String get voce_consegue_usar_o_vaso_sanitario_sozinho_sem_receber_ajuda => 'assets/task_prompts/audio_41.wav';
+  static String get voce_consegue_usar_o_celular_ou_o_telefone_fixo_sozinho_sem_receber_ajuda => 'assets/task_prompts/audio_42.wav';
+  static String get voce_consegue_fazer_compras_sozinho_sem_receber_ajuda => 'assets/task_prompts/audio_43.wav';
+  static String get voce_consegue_administrar_o_seu_proprio_dinheiro_sozinho_sem_receber_ajuda => 'assets/task_prompts/audio_44.wav';
+  static String get voce_consegue_administrar_os_seus_medicamentos_sozinho_sem_receber_ajuda => 'assets/task_prompts/audio_45.wav';
+  static String get voce_consegue_usar_transporte_para_se_deslocar_sozinho_sem_receber_ajuda => 'assets/task_prompts/audio_46.wav';
+  static String get voce_se_sentiu_triste_com_frequencia => 'assets/task_prompts/audio_48.wav';
+  static String get voce_sentiu_cansaco_ou_falta_de_energia_com_frequencia => 'assets/task_prompts/audio_49.wav';
+  static String get voce_teve_dificuldade_para_dormir_ou_dormiu_mais_que_o_habitual => 'assets/task_prompts/audio_50.wav';
+  static String get na_maior_parte_das_vezes_voce_preferiu_ficar_em_casa_ao_inves_de_sair => 'assets/task_prompts/audio_51.wav';
+  static String get voce_se_sentiu_inutil_ou_com_sentimento_de_culpa => 'assets/task_prompts/audio_52.wav';
+  static String get voce_sentiu_que_perdeu_o_interesse_ou_prazer_por_coisas_que_antes_voce_gostava => 'assets/task_prompts/audio_53.wav';
+  static String get voce_tem_esperanca_sobre_o_seu_futuro => 'assets/task_prompts/audio_54.wav';
+  static String get voce_acha_que_vale_a_pena_continuar_vivendo => 'assets/task_prompts/audio_55.wav';
+  static String get obrigado_pela_sua_participacao => 'assets/task_prompts/audio_56.wav';
+
 
   static String get audioPlaceholder => 'assets/audio/audio_placeholder.mp3';
   static String get cincoPatinhos => 'assets/audio/cinco_patinhos.mp3';
